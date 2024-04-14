449faedb41573370aaf2833228bf3435a6184995
raghavrover <raghubabubojjamgari@gmail.com>
2024-04-15 00:42:25 +0530

    commenting the db connection, to find out the error

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ import app from "./src/app.js";
 dotenv.config({ path: "./.env" });
 
 const port = process.env.PORT || 3000;
+console.log("Port from the env vars", process.env.PORT);
+console.log(
+  "MongoDB connection string from the env vars",
+  process.env.MONGODB_URI
+);
 
 const initiateTheServer = async () => {
   try {
-    const connection = await connectDB();
+    // const connection = await connectDB();
     // console.log(connection);
     app.listen(port, () => console.log(`Server is running on ${port}`));
   } catch (error) {
